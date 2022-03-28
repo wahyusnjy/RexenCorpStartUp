dc389231906371a7fe1f9a16398ed8a91c5d1efb
wahyusnjy <wahyusnjy@gmail.com>
2022-03-28 20:03:30 +0700

    Get Campaigns endpoint(WIP)

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,40 @@
 package handler
 
+import (
+	"net/http"
+	"rexencorpstartup/campaign"
+	"rexencorpstartup/helper"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 // tangkap parameter di handler
 // handler ke service
 // service yg menentukan repository mana yang di call
 // repository: GetAll, GetByUserID
 //db
+
+type campaignHandler struct{
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+
+//api/v1/campaigns
+func (h *campaignHandler) GetCampaigns(c *gin.Context){
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error To Get Campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("List Of Campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+
+}
